ee98edaee3c8e53458ce916b7d7db8d5ee6050b2
TobiWeller <89073112+TobiWeller@users.noreply.github.com>
2021-08-31 14:44:32 +0200

    Add files via upload

--- learn_representation.py
+++ learn_representation.py
@@ -2,6 +2,8 @@ import numpy as np
 from RBM import RBM
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
+from pathlib import Path
+import wget
 import argparse
 import os
 
@@ -10,6 +12,37 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('--dataset', nargs='?', default='DBp_2016-04', type=str)
 parser = parser.parse_args()
+
+
+# https://www.dropbox.com/sh/szvuv79ubfqgmn5/AACHxl_eC0frcGrZpVy0VDQPa?dl=0
+
+
+links = {}
+links['dblp'] = 'https://www.dropbox.com/s/78srst5bjt2tta1/dataset.pkl?dl=1'
+links['dbp_type_mapping'] = 'https://www.dropbox.com/s/2ec6dyr90pmjfm9/dbp_type_mapping.json?dl=1'
+links['umls'] = 'https://www.dropbox.com/s/madbrirjc3yjtru/dataset.pkl?dl=1'
+links['Person_DBpedia'] = 'https://www.dropbox.com/s/1omj2btnoj8g4xa/dataset.pkl?dl=1'
+links['DBp_2016-04'] = 'https://www.dropbox.com/s/z38exis1ah3q5ze/dataset.pkl?dl=1'
+links['Company_DBpedia'] = 'https://www.dropbox.com/s/bft3hmk2m6ecrkl/dataset.pkl?dl=1'
+links['Songs_DBpedia'] = 'https://www.dropbox.com/s/u9k6qaydqowckae/dataset.pkl?dl=1'
+links['Books_DBpedia'] = 'https://www.dropbox.com/s/wdqhov2g4bvwzr9/dataset.pkl?dl=1'
+links['ChemicalCompounds_DBpedia'] = 'https://www.dropbox.com/s/fyyqgtwwf2pnj3b/dataset.pkl?dl=1'
+links['Universities_DBpedia'] = 'https://www.dropbox.com/s/0g2moh3puz09uoy/dataset.pkl?dl=1'
+
+
+if not os.path.isfile('./dataset/dbp_type_mapping.json'):
+    print("Downloading dbp_type_mapping data.")
+    data_url = links['dbp_type_mapping']
+    wget.download(data_url, './dataset/dbp_type_mapping.json')
+
+
+if not os.path.isfile('./dataset/{}/dataset.pkl'.format(parser.dataset)):
+    print("Downloading {} data.".format(parser.dataset))
+    data_url = links[parser.dataset]
+    Path('./dataset/{}'.format(parser.dataset)).mkdir(parents=True, exist_ok=True)
+    wget.download(data_url, './dataset/{}/dataset.pkl'.format(parser.dataset))
+
+
 
 print('Learning Ridle Representations on', parser.dataset)
 # Loading Files
